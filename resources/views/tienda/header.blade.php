d63136c201529a3b770e89e796b0fed9369ab965
Raul <neskirbi@gmail.com>
2023-06-19 15:18:20 -0600

    haciendo navigationbar

--- resources/views/tienda/header.blade.php
+++ resources/views/tienda/header.blade.php
@@ -1,5 +1,12 @@
 
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
 <link rel="stylesheet" href="{{asset('assets/css/estilo.css')}}">
+<script src="{{asset('assets/js/metodos.js')}}"></script>
 
 
 <link rel="stylesheet" href="{{asset('assets/fonts/afonts/css/font-awesome.min.css')}}">
